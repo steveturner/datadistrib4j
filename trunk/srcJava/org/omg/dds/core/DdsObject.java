962a8556c1d1bce0fc962a0018f697c7b4dcd465
rick.warren@rti.com <rick.warren@rti.com@e8e814fa-b773-11de-a210-8fd6ca7157a7>
2010-05-08 14:38:20 +0000

    Updated copyright notices

--- trunk/srcJava/org/omg/dds/core/DdsObject.java
+++ trunk/srcJava/org/omg/dds/core/DdsObject.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2009, Real-Time Innovations, Inc.
+/* Copyright (c) 2009-2010, Real-Time Innovations, Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
