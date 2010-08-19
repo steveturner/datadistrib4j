fa169b6b1ae7a8e02dba56f55a73976495931247
rick.warren <rick.warren@rti.com>
2010-08-19 22:09:14 +0000

    Updated copyright notices to mention OMG and PrismTech

--- srcJava/org/omg/dds/type/typeobject/TypeObject.java
+++ srcJava/org/omg/dds/type/typeobject/TypeObject.java
@@ -9,7 +9,7 @@
  * - Redistributions in binary form must reproduce the above copyright
  *   notice, this list of conditions and the following disclaimer in the
  *   documentation and/or other materials provided with the distribution.
- * - Neither the name of Real-Time Innovations, Inc. nor the names of its
+ * - Neither the names of the above copyright holders nor the names of their
  *   contributors may be used to endorse or promote products derived from
  *   this software without specific prior written permission.
  * 
