3898e00e6fa9e3eb0cf0e58d6d5411d5d02a2d7c
rick.warren <rick.warren@rti.com>
2011-05-25 22:44:35 +0000

    OMG issue #16104: adding JavaDoc copied from DDS specification, version 1.2

--- srcJava/org/omg/dds/pub/package-info.java
+++ srcJava/org/omg/dds/pub/package-info.java
@@ -1,6 +1,6 @@
 /**
- * The Publication Module contains the {@link Publisher }and
- * {@link DataWriter} interfaces as well as the {@link PublisherListener }and
+ * The Publication Module contains the {@link Publisher} and
+ * {@link DataWriter} interfaces as well as the {@link PublisherListener} and
  * {@link DataWriterListener} interfaces, and more generally, all that is
  * needed on the publication side.
  */
